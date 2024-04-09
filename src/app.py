2363e6f9e9126e7c5fcbe770ae72263050f7a4e0
nicole bendel <153727790+nicolebendel@users.noreply.github.com>
2024-04-09 22:07:12 +0000

    porfavor funciona

--- src/app.py
+++ src/app.py
@@ -4,13 +4,14 @@ import pandas as pd
 from dotenv import load_dotenv
 
 # load the .env file variables
+
 load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
+
 def connect():
-    global engine # Esto nos permite usar una variable global llamada motor
-    # Una "cadena de conexión" es básicamente una cadena que contiene todas las credenciales de la base de datos juntas
-    connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    global engine 
+    connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}?"
     print("Starting the connection...")
     engine = create_engine(connection_string)
     engine.connect()
@@ -20,7 +21,8 @@ engine = connect()
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
-engine.execute("""CREATE TABLE publishers(
+engine.execute("""
+CREATE TABLE publishers(
     publisher_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY(publisher_id)
@@ -52,15 +54,15 @@ CREATE TABLE book_authors (
     PRIMARY KEY(book_id, author_id),
     CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
     CONSTRAINT fk_author FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
-);""")
+);
+""")
+
+
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
 
-engine.execute("""DROP TABLE book_authors;
-DROP TABLE books;
-DROP TABLE authors;
-DROP TABLE publishers;""")
 
-engine.execute("""INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
+engine.execute("""
+INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
 INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
 INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS');
 INSERT INTO publishers(publisher_id, name) VALUES (4, 'Academic Press');
@@ -68,7 +70,6 @@ INSERT INTO publishers(publisher_id, name) VALUES (5, 'Addison Wesley');
 INSERT INTO publishers(publisher_id, name) VALUES (6, 'Albert&Sweigart');
 INSERT INTO publishers(publisher_id, name) VALUES (7, 'Alfred A. Knopf');
 
--- authors 
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (1, 'Merritt', null, 'Eric');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (2, 'Linda', null, 'Mui');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (3, 'Alecos', null, 'Papadatos');
@@ -78,7 +79,6 @@ INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (6, '
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (7, 'Yuval', 'Noah', 'Harari');
 INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (8, 'Paul', null, 'Albitz');
 
--- books
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (1, 'Lean Software Development: An Agile Toolkit', 240, 4.17, '9780320000000', '2003-05-18', 5);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (2, 'Facing the Intelligence Explosion', 91, 3.87, null, '2013-02-01', 7);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (3, 'Scala in Action', 419, 3.74, '9781940000000', '2013-04-10', 1);
@@ -90,7 +90,6 @@ INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, pu
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (9, 'The Apollo Guidance Computer: Architecture And Operation (Springer Praxis Books / Space Exploration)', 439, 4.29, '9781440000000', '2010-07-01', 6);
 INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (10, 'Minds and Computers: An Introduction to the Philosophy of Artificial Intelligence', 222, 3.54, '9780750000000', '2007-02-13', 7);
 
--- book authors
 INSERT INTO book_authors (book_id, author_id) VALUES (1, 1);
 INSERT INTO book_authors (book_id, author_id) VALUES (2, 8);
 INSERT INTO book_authors (book_id, author_id) VALUES (3, 7);
@@ -100,8 +99,11 @@ INSERT INTO book_authors (book_id, author_id) VALUES (6, 4);
 INSERT INTO book_authors (book_id, author_id) VALUES (7, 3);
 INSERT INTO book_authors (book_id, author_id) VALUES (8, 2);
 INSERT INTO book_authors (book_id, author_id) VALUES (9, 4);
-INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);""")
+INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);
+""")
+
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
+
 
 df = pd.read_sql("SELECT * FROM authors", engine)
 print(df)  
